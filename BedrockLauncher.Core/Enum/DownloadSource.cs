cc57842a70b13195f579de299d5fe7ccac2ef316
Minecraft一角钱 <liy_0924@qq.com>
2025-08-31 17:54:44 +0800

    feat: add download enums

---
+++ BedrockLauncher.Core/Enum/DownloadSource.cs
@@ -0,0 +1,14 @@
+﻿namespace BedrockLauncher.Core.Enum;
+
+public enum DownloadSource
+{
+    /// <summary>
+    /// 微软服务器
+    /// </summary>
+    Microsoft,
+
+    /// <summary>
+    /// McAppx 服务器
+    /// </summary>
+    McAppx
+}
