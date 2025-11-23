19cbb59b8e783744b89fb48a47fb948b354dc0b9
Dime <liy_0924@qq.com>
2025-11-23 09:10:02 +0800

    feat: 格式化代码

--- BedrockLauncher.Core/InstallCallback.cs
+++ BedrockLauncher.Core/InstallCallback.cs
@@ -13,26 +13,29 @@ namespace BedrockLauncher.Core
         /// <summary>
         /// 取消token
         /// </summary>
-        public CancellationToken CancellationToken { get; set; } =   new CancellationToken();
+        public CancellationToken CancellationToken { get; set; } = new CancellationToken();
 
         /// <summary>
         /// 下载进度
         /// </summary>
         public Progress<DownloadProgress> downloadProgress { get; set; } =
             new Progress<DownloadProgress>((progress => { }));
+
         /// <summary>
         /// 部署进度
         /// </summary>
-        public required Action<string, uint> registerProcess_percent { get; set; } 
+        public required Action<string, uint> registerProcess_percent { get; set; }
+
         /// <summary>
         /// 结果返回
         /// </summary>
         public required Action<AsyncStatus, Exception> result_callback { get; set; }
 
-        public  Progress<ZipProgress> zipProgress { get; set; } = new Progress<ZipProgress>((progress => { }));
+        public Progress<ZipProgress> zipProgress { get; set; } = new Progress<ZipProgress>((progress => { }));
+
         /// <summary>
         /// 安装状态
         /// </summary>
-        public Action<InstallStates> install_states { get; set; } = new((states => {}));
+        public Action<InstallStates> install_states { get; set; } = new((states => { }));
     }
 }
